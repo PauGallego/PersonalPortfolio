367f8fc5ad03f091d17ca203e4a83ebe1488f283
Pau Gallego <paugf2004@gmail.com>
2024-02-14 10:22:17 +0100

    sisisis

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+import { addDynamicIconSelectors } from '@iconify/tailwind'
+
 export default {
+
+	
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [addDynamicIconSelectors()]
 }
